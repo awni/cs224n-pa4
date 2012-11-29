e1bb37311c0e917a82cbc70fdd36decdbdd6f9ab
Ibrahim Cotran <ibrahimcotran@gmail.com>
2012-11-29 14:13:15 -0800

    adding results

--- java/iterate.py
+++ java/iterate.py
@@ -1,35 +1,19 @@
 #!/usr/bin/env python
 
-import difflib
-import re
 import os
-import sys
-import traceback
 
 if __name__ == "__main__":
     # regularization constant
-    C = ["0", "0.01", "0.001", "0.0001", "0.00001"]
+    C = ["0", "0.01"]
     
     # window size
-    W = ["1", "3", "5", "7", "9", "11", "13", "15", "17"]
+    W = ["7", "9", "11"]
     
     # hidden layer size
-    H = ["100", "200", "400", "600", "800", "1000"]
+    H = ["200", "800"]
 
     # learning
-    A = ["0.01", "0.001", "0.0001"]
-    
-    # regularization constant
-    C = ["0"]
-    
-    # window size
-    W = ["1"]
-    
-    # hidden layer size
-    H = ["3"]
-
-    # learning
-    A = ["0.01"]
+    A = ["0.01", "0.001"]
     
     command = "java -cp extlib/ejml.jar:classes -Xmx4G cs224n.deep.NER ../data/train ../data/dev -print %s %s %s %s"
     for i in C:
@@ -41,4 +25,6 @@ if __name__ == "__main__":
                     results = "C = %s W = %s H = %s A = %s \n" % (i, j, k, l)
                     for line in f:
                         results += line
-                    print results
+                    with open("results.txt", "a") as file:
+                        file.write(results)
+                        file.write("\n")
